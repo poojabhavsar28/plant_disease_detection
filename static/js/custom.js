7a8db4c35c67d732e53188dcb5aa3971a966841a
Pooja Bhavsar <poojabhavsar036@gmail.com>
2026-06-17 01:32:19 +0530

    added js folder and files

---
+++ static/js/custom.js
@@ -0,0 +1,37 @@
+$('.dealslider .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+
+$('.testi_slider .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
